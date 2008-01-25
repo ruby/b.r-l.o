ruby/b.r-l.o

commit 1d22e1f0190b0e3c4fcc4e733e5660cd9d869b73

Show explicit error message when the scm command failed (eg. when svn binary is not available).
git-svn-id: http://redmine.rubyforge.org/svn/trunk@1094 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/repositories_controller.rb
@@ -56,6 +56,8 @@ class RepositoriesController < ApplicationController
     # latest changesets
     @changesets = @repository.changesets.find(:all, :limit => 10, :order => "committed_on DESC")
     show_error and return unless @entries || @changesets.any?
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def browse
@@ -65,12 +67,16 @@ class RepositoriesController < ApplicationController
     else
       show_error unless @entries
     end
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def changes
     @entry = @repository.scm.entry(@path, @rev)
     show_error and return unless @entry
     @changesets = @repository.changesets_for_path(@path)
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def revisions
@@ -97,11 +103,15 @@ class RepositoriesController < ApplicationController
       # Prevent empty lines when displaying a file with Windows style eol
       @content.gsub!("\r\n", "\n")
     end
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def annotate
     @annotate = @repository.scm.annotate(@path, @rev)
     show_error and return if @annotate.nil? || @annotate.empty?
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def revision
@@ -119,6 +129,8 @@ class RepositoriesController < ApplicationController
     end
   rescue ChangesetNotFound
     show_error
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def diff
@@ -137,6 +149,8 @@ class RepositoriesController < ApplicationController
       @diff = @repository.diff(@path, @rev, @rev_to, @diff_type)
       show_error and return unless @diff
     end
+  rescue Redmine::Scm::Adapters::CommandFailed => e
+    show_error_command_failed(e.message)
   end
   
   def stats  
@@ -176,9 +190,12 @@ private
     render_404
   end
 
-  def show_error
-    flash.now[:error] = l(:notice_scm_error)
-    render :nothing => true, :layout => true
+  def show_error_not_found
+    render_error l(:error_scm_not_found)
+  end
+  
+  def show_error_command_failed(msg)
+    render_error l(:error_scm_command_failed, msg)
   end
   
   def graph_commits_per_month(repository)

app/controllers/application.rb
@@ -137,6 +137,11 @@ class ApplicationController < ActionController::Base
     return false
   end
   
+  def render_error(msg)
+    flash.now[:error] = msg
+    render :nothing => true, :layout => !request.xhr?, :status => 500
+  end
+  
   def render_feed(items, options={})    
     @items = items || []
     @items.sort! {|x,y| y.event_datetime <=> x.event_datetime }

lang/bg.yml
@@ -68,11 +68,13 @@ notice_successful_delete: Успешно изтриване.
 notice_successful_connection: Успешно свързване.
 notice_file_not_found: Несъществуваща или преместена страница.
 notice_locking_conflict: Друг потребител променя тези данни в момента.
-notice_scm_error: Несъществуващ обект в склада.
 notice_not_authorized: Нямате право на достъп до тази страница.
 notice_email_sent: Изпратен e-mail на %s
 notice_email_error: Грешка при изпращане на e-mail (%s)
 notice_feeds_access_key_reseted: Вашия ключ за RSS достъп беше променен.
+
+error_scm_not_found: Несъществуващ обект в склада.
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Вашата парола
 mail_body_lost_password: 'За да смените паролата си, използвайте следния линк:'

lang/cs.yml
@@ -68,11 +68,13 @@ notice_successful_delete: Úspěšné smazání.
 notice_successful_connection: Úspěšné připojení.
 notice_file_not_found: Stránka na kterou se snažíte zobrazit neexistuje nebo byla smazána.
 notice_locking_conflict: Údaje byly změněny jiným uživatelem.
-notice_scm_error: Entry and/or revision doesn't exist in the repository.
 notice_not_authorized: Nemáte dostatečná práva pro zobrazení této stránky.
 notice_email_sent: Na adresu %s byl odeslán email
 notice_email_error: Při odesílání emailu nastala chyba (%s)
 notice_feeds_access_key_reseted: Váš klíč pro přístup k RSS byl resetován.
+
+error_scm_not_found: "Entry and/or revision doesn't exist in the repository."
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Vaše heslo
 mail_body_lost_password: 'To change your Redmine password, click on the following link:'

lang/de.yml
@@ -68,11 +68,13 @@ notice_successful_delete: Erfolgreich gelöscht.
 notice_successful_connection: Verbindung erfolgreich.
 notice_file_not_found: Anhang besteht nicht oder ist gelöscht worden.
 notice_locking_conflict: Datum wurde von einem anderen Benutzer geändert.
-notice_scm_error: Eintrag und/oder Revision besteht nicht im Projektarchiv.
 notice_not_authorized: Sie sind nicht berechtigt, auf diese Seite zuzugreifen.
 notice_email_sent: Eine E-Mail wurde an %s gesendet.
 notice_email_error: Beim Senden einer E-Mail ist ein Fehler aufgetreten (%s).
 notice_feeds_access_key_reseted: Ihr RSS-Zugriffsschlüssel wurde zurückgesetzt.
+
+error_scm_not_found: "Eintrag und/oder Revision besteht nicht im Projektarchiv."
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Ihr Redmine-Kennwort
 mail_body_lost_password: 'Benutzen Sie den folgenden Link, um Ihr Kennwort zu ändern:'

lang/en.yml
@@ -68,7 +68,6 @@ notice_successful_delete: Successful deletion.
 notice_successful_connection: Successful connection.
 notice_file_not_found: The page you were trying to access doesn't exist or has been removed.
 notice_locking_conflict: Data have been updated by another user.
-notice_scm_error: Entry and/or revision doesn't exist in the repository.
 notice_not_authorized: You are not authorized to access this page.
 notice_email_sent: An email was sent to %s
 notice_email_error: An error occurred while sending mail (%s)
@@ -79,6 +78,8 @@ notice_account_pending: "Your account was created and is now pending administrat
 notice_default_data_loaded: Default configuration successfully loaded.
 
 error_can_t_load_default_data: "Default configuration could not be loaded: %s"
+error_scm_not_found: "Entry and/or revision doesn't exist in the repository."
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Your Redmine password
 mail_body_lost_password: 'To change your Redmine password, click on the following link:'

lang/es.yml
@@ -68,8 +68,10 @@ notice_successful_delete: Borrado correcto.
 notice_successful_connection: Conexión correcta.
 notice_file_not_found: La página a la que intentas acceder no existe.
 notice_locking_conflict: Los datos han sido modificados por otro usuario.
-notice_scm_error: La entrada y/o la revisión no existe en el repositorio.
 notice_not_authorized: No tiene autorización para acceder a esta página.
+
+error_scm_not_found: "La entrada y/o la revisión no existe en el repositorio."
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Tu contraseña del CIYAT - Gestor de Solicitudes
 mail_body_lost_password: 'Para cambiar su contraseña de Redmine, haga click en el siguiente enlace:'

lang/fi.yml
@@ -68,7 +68,6 @@ notice_successful_delete: Poisto onnistui.
 notice_successful_connection: Yhteyden muodostus onnistui.
 notice_file_not_found: Hakemaasi sivua ei löytynyt tai se on poistettu.
 notice_locking_conflict: Toinen käyttäjä on päivittänyt tiedot.
-notice_scm_error: Syötettä ja/tai versiota ei löydy säiliöstä.
 notice_not_authorized: Sinulla ei ole oikeutta näyttää tätä sivua.
 notice_email_sent: Sähköposti on lähetty osoitteeseen %s
 notice_email_error: Sähköpostilähetyksessä tapahtui virhe (%s)
@@ -79,6 +78,8 @@ notice_account_pending: "Tilisi on luotu ja odottaa ylläpitäjän hyväksyntä�
 notice_default_data_loaded: Vakio asetusten palautus onnistui.
 
 error_can_t_load_default_data: "Vakio asetuksia ei voitu ladata: %s"
+error_scm_not_found: "Syötettä ja/tai versiota ei löydy säiliöstä."
+error_scm_command_failed: "An error occurred when trying to access the repository: %s"
 
 mail_subject_lost_password: Sinun Redmine salasanasi
 mail_body_lost_password: 'Vaihtaaksesi Redmine salasanasi, paina seuraavaa linkkiä:'

commit 18e7788f26709d0ac2549ee86b81589d040e96fd

Fixed RepositoriesController: undefined local variable or method `show_error' (broken by r1094).
git-svn-id: http://redmine.rubyforge.org/svn/trunk@1104 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/repositories_controller.rb
@@ -55,7 +55,7 @@ class RepositoriesController < ApplicationController
     @entries = @repository.entries('')    
     # latest changesets
     @changesets = @repository.changesets.find(:all, :limit => 10, :order => "committed_on DESC")
-    show_error and return unless @entries || @changesets.any?
+    show_error_not_found unless @entries || @changesets.any?
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)
   end
@@ -65,7 +65,7 @@ class RepositoriesController < ApplicationController
     if request.xhr?
       @entries ? render(:partial => 'dir_list_content') : render(:nothing => true)
     else
-      show_error unless @entries
+      show_error_not_found unless @entries
     end
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)
@@ -73,7 +73,7 @@ class RepositoriesController < ApplicationController
   
   def changes
     @entry = @repository.scm.entry(@path, @rev)
-    show_error and return unless @entry
+    show_error_not_found and return unless @entry
     @changesets = @repository.changesets_for_path(@path)
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)
@@ -96,7 +96,7 @@ class RepositoriesController < ApplicationController
   
   def entry
     @content = @repository.scm.cat(@path, @rev)
-    show_error and return unless @content
+    show_error_not_found and return unless @content
     if 'raw' == params[:format]      
       send_data @content, :filename => @path.split('/').last
     else
@@ -109,7 +109,7 @@ class RepositoriesController < ApplicationController
   
   def annotate
     @annotate = @repository.scm.annotate(@path, @rev)
-    show_error and return if @annotate.nil? || @annotate.empty?
+    show_error_not_found and return if @annotate.nil? || @annotate.empty?
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)
   end
@@ -128,7 +128,7 @@ class RepositoriesController < ApplicationController
       format.js {render :layout => false}
     end
   rescue ChangesetNotFound
-    show_error
+    show_error_not_found
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)
   end
@@ -147,7 +147,7 @@ class RepositoriesController < ApplicationController
     @cache_key = "repositories/diff/#{@repository.id}/" + Digest::MD5.hexdigest("#{@path}-#{@rev}-#{@rev_to}-#{@diff_type}")    
     unless read_fragment(@cache_key)
       @diff = @repository.diff(@path, @rev, @rev_to, @diff_type)
-      show_error and return unless @diff
+      show_error_not_found unless @diff
     end
   rescue Redmine::Scm::Adapters::CommandFailed => e
     show_error_command_failed(e.message)

test/functional/repositories_subversion_controller_test.rb
@@ -67,6 +67,12 @@ class RepositoriesSubversionControllerTest < Test::Unit::TestCase
       assert_response :success
       assert_template 'entry'
     end
+    
+    def test_entry_not_found
+      get :entry, :id => 1, :path => ['subversion_test', 'zzz.c']
+      assert_tag :tag => 'div', :attributes => { :class => /error/ },
+                                :content => /Entry and\/or revision doesn't exist/
+    end
   
     def test_entry_download
       get :entry, :id => 1, :path => ['subversion_test', 'helloworld.c'], :format => 'raw'