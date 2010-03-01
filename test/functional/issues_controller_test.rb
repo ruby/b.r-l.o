ruby/b.r-l.o

commit 98c18a588bf64c21837e19e476696cd30fa609fb

Refactor: Move the updating of an Issue to the #update method.
git-svn-id: http://redmine.rubyforge.org/svn/trunk@3486 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -198,6 +198,31 @@ class IssuesController < ApplicationController
       @issue.safe_attributes = attrs
     end
 
+    respond_to do |format|
+      format.html { }
+      format.xml  { }
+    end
+  end
+
+  #--
+  # Start converting to the Rails REST controllers
+  #++
+  def update
+    @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
+    @priorities = IssuePriority.all
+    @edit_allowed = User.current.allowed_to?(:edit_issues, @project)
+    @time_entry = TimeEntry.new
+    
+    @notes = params[:notes]
+    journal = @issue.init_journal(User.current, @notes)
+    # User can change issue attributes only if he has :edit permission or if a workflow transition is allowed
+    if (@edit_allowed || !@allowed_statuses.empty?) && params[:issue]
+      attrs = params[:issue].dup
+      attrs.delete_if {|k,v| !UPDATABLE_ATTRS_ON_TRANSITION.include?(k) } unless @edit_allowed
+      attrs.delete(:status_id) unless @allowed_statuses.detect {|s| s.id.to_s == attrs[:status_id].to_s}
+      @issue.safe_attributes = attrs
+    end
+
     if request.get?
       # nop
     else
@@ -235,13 +260,6 @@ class IssuesController < ApplicationController
     flash.now[:error] = l(:notice_locking_conflict)
     # Remove the previously added attachments if issue was not updated
     attachments.each(&:destroy)
-  end
-
-  #--
-  # Start converting to the Rails REST controllers
-  #++
-  def update
-    edit
   end
 
   def reply

config/routes.rb
@@ -126,7 +126,8 @@ ActionController::Routing::Routes.draw do |map|
       issues_actions.connect 'issues.:format', :action => 'new', :format => /xml/
     end
     issues_routes.with_options :conditions => {:method => :put} do |issues_actions|
-      issues_actions.connect 'issues/:id.:format', :action => 'edit', :id => /\d+/, :format => /xml/
+      issues_actions.connect 'issues/:id/edit', :action => 'update', :id => /\d+/
+      issues_actions.connect 'issues/:id.:format', :action => 'update', :id => /\d+/, :format => /xml/
     end
     issues_routes.with_options :conditions => {:method => :delete} do |issues_actions|
       issues_actions.connect 'issues/:id.:format', :action => 'destroy', :id => /\d+/, :format => /xml/

test/integration/issues_api_test.rb
@@ -114,7 +114,7 @@ class IssuesApiTest < ActionController::IntegrationTest
   def test_update_routing
     assert_routing(
       {:method => :put, :path => '/issues/1.xml'},
-      :controller => 'issues', :action => 'edit', :id => '1', :format => 'xml'
+      :controller => 'issues', :action => 'update', :id => '1', :format => 'xml'
     )
   end
   

test/integration/issues_test.rb
@@ -69,7 +69,7 @@ class IssuesTest < ActionController::IntegrationTest
     log_user('jsmith', 'jsmith')
     set_tmp_attachments_directory
 
-    post 'issues/1/edit',
+    put 'issues/1/edit',
          :notes => 'Some notes',
          :attachments => {'1' => {'file' => uploaded_test_file('testfile.txt', 'text/plain'), 'description' => 'This is an attachment'}}
     assert_redirected_to "issues/1"

commit 184a017eba1c8247e0a9d18346ada6de24bfd4c8

Verify issues are updated by HTTP PUT only.  Regression from r3486.
git-svn-id: http://redmine.rubyforge.org/svn/trunk@3520 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/controllers/issues_controller.rb
@@ -50,7 +50,9 @@ class IssuesController < ApplicationController
   verify :method => [:post, :delete],
          :only => :destroy,
          :render => { :nothing => true, :status => :method_not_allowed }
-           
+
+  verify :method => :put, :only => :update, :render => {:nothing => true, :status => :method_not_allowed }
+  
   def index
     retrieve_query
     sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)

test/functional/issues_controller_test.rb
@@ -657,6 +657,20 @@ class IssuesControllerTest < ActionController::TestCase
     assert_select_rjs :show, "update"
   end
 
+  def test_update_using_invalid_http_verbs
+    @request.session[:user_id] = 2
+    subject = 'Updated by an invalid http verb'
+
+    get :update, :id => 1, :issue => {:subject => subject}
+    assert_not_equal subject, Issue.find(1).subject
+
+    post :update, :id => 1, :issue => {:subject => subject}
+    assert_not_equal subject, Issue.find(1).subject
+
+    delete :update, :id => 1, :issue => {:subject => subject}
+    assert_not_equal subject, Issue.find(1).subject
+  end
+
   def test_put_update_without_custom_fields_param
     @request.session[:user_id] = 2
     ActionMailer::Base.deliveries.clear