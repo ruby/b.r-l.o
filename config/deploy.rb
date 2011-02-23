4f3ddfe3a4316e489e473c97de6f0fe7480d3a92
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2011-02-23 20:55:19 +0900

    applied vlad

---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+set :application, 'redmine'
+set :domain, 'fluorine.ruby-lang.org'
+set :deploy_to, '/var/lib/redmine'
+set :repository, 'git://github.com/yugui/redmine4ruby-lang.git'
+
+set :revision, 'origin/ruby-lang.org'
+
+shared_paths.merge!({
+  'config/database.yml' => 'config/database.yml',
+  'config/additional_environment.rb' => 'config/additional_environment.rb',
+  'config/imap.yaml' => 'config/imap.yaml',
+  'files' => 'files',
+  'backup' => 'backup',
+})
