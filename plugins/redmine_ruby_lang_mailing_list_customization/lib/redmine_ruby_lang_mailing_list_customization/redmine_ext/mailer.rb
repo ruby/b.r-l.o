181f7eb5e63b81c3215deaaa07644cd49a8ec126
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2011-03-27 20:18:12 +0900

    get rid of "unspecified-recipient". make recipients bcc by an ad-hoc way

--- plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
+++ plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
@@ -13,6 +13,15 @@ Mailer.class_eval do
   alias_method_chain :issue_edit, :ruby_lang_mailing_list_customization
 
   private
+  def create_mail_with_ruby_lang_mailing_list_customization
+    bcc = cc
+    cc []
+    create_mail_without_ruby_lang_mailing_list_customization
+  end
+  alias_method_chain :create_mail, :ruby_lang_mailing_list_customization
+
+
+  private
   def update_from(user)
     if user.anonymous? or user.preference.try(:hide_mail?)
       from name_addr(user.name, Setting.mail_from)
