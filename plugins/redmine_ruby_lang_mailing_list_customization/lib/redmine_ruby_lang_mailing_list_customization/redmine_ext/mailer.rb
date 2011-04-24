7002c3691d6e9908bd90a5cb0d8bcbc262e121fb
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2011-04-24 21:09:29 +0900

    Revert "removes a feature that rewrites From address in notification."
    This reverts commit 8954efefde899bc0b9a37de19a2bf2d3021ef6d5.
    
    naruse fixed the problem by allowing mail from redmine to
    ruby-core/ruby-dev. http://twitter.com/#!/nalsh/status/60742848156401665

--- plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
+++ plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
@@ -1,4 +1,17 @@
 Mailer.class_eval do
+  def issue_add_with_ruby_lang_mailing_list_customization(issue)
+    issue_add_without_ruby_lang_mailing_list_customization(issue)
+    update_from(issue.author)
+  end
+
+  def issue_edit_with_ruby_lang_mailing_list_customization(journal)
+    issue_edit_without_ruby_lang_mailing_list_customization(journal)
+    update_from(journal.user)
+  end
+
+  alias_method_chain :issue_add, :ruby_lang_mailing_list_customization
+  alias_method_chain :issue_edit, :ruby_lang_mailing_list_customization
+
   private
   def create_mail_with_ruby_lang_mailing_list_customization
     bcc = cc
@@ -6,5 +19,21 @@ Mailer.class_eval do
     create_mail_without_ruby_lang_mailing_list_customization
   end
   alias_method_chain :create_mail, :ruby_lang_mailing_list_customization
+
+
+  private
+  def update_from(user)
+    if user.anonymous? or user.preference.try(:hide_mail?)
+      from name_addr(user.name, Setting.mail_from)
+    else
+      from name_addr(user.name, user.mail)
+    end
+  end
+
+  def name_addr(name, addr_spec)
+    addr = TMail::Address.parse(addr_spec)
+    addr.name = name.to_s
+    return addr.to_s
+  end
 end
 
