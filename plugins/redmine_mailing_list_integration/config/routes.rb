c01cc12dcc2c9cb296ec161c1b0cff69180eb37b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-05-13 11:12:04 +0900

    upgrade routes.rb to Rails 3 style

--- plugins/redmine_mailing_list_integration/config/routes.rb
+++ plugins/redmine_mailing_list_integration/config/routes.rb
@@ -1,6 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :mailing_lists do |ml| 
-    ml.resources :uses, :controller => 'uses_of_mailing_list'
+RedmineApp::Application.routes.draw do
+  resources :mailing_lists do
+    resources :uses, :controller => 'uses_of_mailing_list'
   end
 end
-
