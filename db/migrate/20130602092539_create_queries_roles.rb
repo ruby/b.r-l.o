889c0509a10e4105b69a0b84159fbbd766d6ac72
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-12-23 16:38:23 +0900

    ignored to applied migrations already.

--- db/migrate/20130602092539_create_queries_roles.rb
+++ db/migrate/20130602092539_create_queries_roles.rb
@@ -1,10 +1,10 @@
 class CreateQueriesRoles < ActiveRecord::Migration
   def self.up
-    create_table :queries_roles, :id => false do |t|
-      t.column :query_id, :integer, :null => false
-      t.column :role_id, :integer, :null => false
-    end
-    add_index :queries_roles, [:query_id, :role_id], :unique => true, :name => :queries_roles_ids
+    # create_table :queries_roles, :id => false do |t|
+    #   t.column :query_id, :integer, :null => false
+    #   t.column :role_id, :integer, :null => false
+    # end
+    # add_index :queries_roles, [:query_id, :role_id], :unique => true, :name => :queries_roles_ids
   end
 
   def self.down
