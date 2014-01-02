74c1601aba4cfa393859273b39a7b709c40397bc
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-01-02 21:23:42 +0900

    fix incompatible api changes

--- plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/mailer.rb
+++ plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/mailer.rb
@@ -10,58 +10,41 @@ Mailer.class_eval do
         super(journal)
       end
     end
-
-#    alias_method_chain :deliver_issue_add, :mailing_list_integration
-#    alias_method_chain :deliver_issue_edit, :mailing_list_integration
   end
 
-  def issue_add_with_mailing_list_integration(issue)
-    issue_add_without_mailing_list_integration(issue)
+  def issue_add_with_mailing_list_integration(issue, to_users, cc_users)
+    issue_add_without_mailing_list_integration(issue, to_users, cc_users)
 
     mailing_lists = issue.project.mail_routes_for_issue(issue)
     record_message(issue, nil, mailing_lists)
 
-    self.cc += recipients
-    self.recipients = mailing_lists.map(&:address)
+    mail :to => mailing_lists.map(&:address),
+      :subject => "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] (#{issue.status.name}) #{issue.subject}"
   end
+  alias_method_chain :issue_add, :mailing_list_integration
 
-  def issue_edit_with_mailing_list_integration(journal)
-    issue_edit_without_mailing_list_integration(journal)
+  def issue_edit_with_mailing_list_integration(journal, to_users, cc_users)
+    issue_edit_without_mailing_list_integration(journal, to_users, cc_users)
 
     issue = journal.issue
     mailing_lists = issue.project.mail_routes_for_issue(issue)
     record_message(issue, journal, mailing_lists)
 
-    self.cc += recipients
-    self.recipients = mailing_lists.map(&:address)
+    s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
+    s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
+    s << issue.subject
+
+    mail :to => mailing_lists.map(&:address),
+      :subject => s
   end
-  alias_method_chain :issue_add, :mailing_list_integration
   alias_method_chain :issue_edit, :mailing_list_integration
-
-  [
-    [ 'document', 'added' ],
-    [ 'news', 'added' ],
-    [ 'message', 'posted' ],
-    [ 'wiki_content', 'added', 'updated' ],
-  ].each do |obj, *events|
-    events.each do |event|
-      define_method("#{obj}_#{event}_with_mailing_list_integration") do |*args|
-        send "#{obj}_#{event}_without_mailing_list_integration", *args
-
-        mailing_lists = args[0].project.send("mail_routes_for_#{obj}", args[0])
-        send("record_message_on_issue_#{event}", args[0], mailing_lists) if obj == 'issue'
-
-        self.cc += recipients
-        self.recipients = mailing_lists.map(&:address)
-      end
-      alias_method_chain "#{obj}_#{event}", :mailing_list_integration
-    end
-  end
 
   def attachments_added_with_mailing_list_integration(attachments)
     attachments_added_without_mailing_list_integration(attachments)
-    self.cc += recipients
-    self.recipients = attatchments.first.container.project.mail_routes_for_attachments(attachments).map(&:address)
+
+    mailing_lists = attatchments.first.container.project.mail_routes_for_attachments(attachments)
+    mail :to => mailing_lists.map(&:address),
+      :subject => "[#{container.project.name}] #{l(:label_attachment_new)}"
   end
   alias_method_chain :attachments_added, :mailing_list_integration
 
