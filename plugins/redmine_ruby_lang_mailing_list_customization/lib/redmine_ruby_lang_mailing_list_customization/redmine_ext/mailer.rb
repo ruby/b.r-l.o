2b19133e95f09f49f844468304749a12b76228c9
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-01-03 11:05:08 +0900

    fix wrong arguments

--- plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
+++ plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
@@ -13,10 +13,10 @@ Mailer.class_eval do
 
   private
 
-  def mail_with_ruby_lang_mailing_list_customization
+  def mail_with_ruby_lang_mailing_list_customization(headers)
     headers[:bcc] = headers[:cc]
     headers[:cc] = []
-    create_mail_without_ruby_lang_mailing_list_customization
+    create_mail_without_ruby_lang_mailing_list_customization(headers)
   end
   alias_method_chain :mail, :ruby_lang_mailing_list_customization
 
