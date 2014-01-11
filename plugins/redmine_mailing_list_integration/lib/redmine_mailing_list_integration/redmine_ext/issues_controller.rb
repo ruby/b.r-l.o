4c6202af5a2543d51fbf689ab85826ab7a1f12cd
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-01-11 11:35:03 +0900

    fix wrong usage of redmine 2.4

--- plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/issues_controller.rb
+++ plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/issues_controller.rb
@@ -1,8 +1,10 @@
 IssuesController.class_eval do
   private
   def update_issue_from_params_with_mailing_list_integration
-    update_issue_from_params_without_mailing_list_integration
-    @issue.current_journal.originates_from_mail = false
+    if val = update_issue_from_params_without_mailing_list_integration
+      @issue.current_journal.originates_from_mail = false
+    end
+    val
   end
 
   alias_method_chain :update_issue_from_params, :mailing_list_integration
