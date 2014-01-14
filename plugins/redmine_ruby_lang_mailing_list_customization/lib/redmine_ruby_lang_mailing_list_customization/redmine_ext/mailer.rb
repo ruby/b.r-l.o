54258801627ed8ad422480b4d9f7ddc7d8b0a5dd
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-01-14 14:27:36 +0900

    specified locale from mailing-list

--- plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
+++ plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
@@ -18,7 +18,8 @@ Mailer.class_eval do
   def mail_with_ruby_lang_mailing_list_customization(headers)
     headers[:bcc] = headers[:cc]
     headers[:cc] = []
-    mail_without_ruby_lang_mailing_list_customization(headers)
+    locale = headers[:to].to_s.include?('ruby-dev') ? :ja : :en
+    I18n.with_locale(locale) { mail_without_ruby_lang_mailing_list_customization(headers) }
   end
   alias_method_chain :mail, :ruby_lang_mailing_list_customization
 end
