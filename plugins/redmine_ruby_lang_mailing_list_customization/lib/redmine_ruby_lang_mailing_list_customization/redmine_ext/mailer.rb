38161fcb829e4e02d604b5b22d2b19ae72531197
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-01-14 13:01:14 +0900

    use mail address of user account

--- plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
+++ plugins/redmine_ruby_lang_mailing_list_customization/lib/redmine_ruby_lang_mailing_list_customization/redmine_ext/mailer.rb
@@ -1,4 +1,18 @@
 Mailer.class_eval do
+  def issue_add_with_ruby_lang_mailing_list_customization(*args)
+    m = issue_add_without_ruby_lang_mailing_list_customization(*args)
+    m.header[:from] = args[0].author.mail # args[0] == issue
+    m
+  end
+  alias_method_chain :issue_add, :ruby_lang_mailing_list_customization
+
+  def issue_edit_with_ruby_lang_mailing_list_customization(*args)
+    m = issue_edit_without_ruby_lang_mailing_list_customization(*args)
+    m.header[:from] = args[0].user.mail # args[0] == journal
+    m
+  end
+  alias_method_chain :issue_edit, :ruby_lang_mailing_list_customization
+
   private
 
   def mail_with_ruby_lang_mailing_list_customization(headers)
