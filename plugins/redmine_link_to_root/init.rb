2f7e67f49ac74c6a2db616b048d5d3b776feaf0d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-03-04 21:16:31 +0900

    update plugin description

--- plugins/redmine_link_to_root/init.rb
+++ plugins/redmine_link_to_root/init.rb
@@ -6,9 +6,9 @@ Redmine::Plugin.register :redmine_link_to_root do
   author 'Yuki Sonoda (Yugui)'
   description <<-EOS.gsub(/^\s+/, '')
     This plugin makes the header title linked to the root of site when it is not associated with any particular project.
-    This plugin is developed for http://redmine.ruby-lang.org.
+    This plugin is developed for https://bugs.ruby-lang.org.
   EOS
-  version '0.0.1'
-  url 'http://github.com/yugui/redmine_link_to_root'
+  version '0.1.0'
+  url 'https://github.com/ruby/redmine_link_to_root'
   author_url 'http://yugui.jp'
 end
