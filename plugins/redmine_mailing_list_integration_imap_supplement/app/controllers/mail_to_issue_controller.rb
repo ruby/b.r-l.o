79ade23d33d8f6706d12c7f368329186a93830f1
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-05-31 22:04:22 +0900

    remove deprecated methods

--- plugins/redmine_mailing_list_integration_imap_supplement/app/controllers/mail_to_issue_controller.rb
+++ plugins/redmine_mailing_list_integration_imap_supplement/app/controllers/mail_to_issue_controller.rb
@@ -21,7 +21,7 @@ class MailToIssueController < ApplicationController
 
       number = "[#{ml.identifier}:#{@mail_to_issue.mail_number}]"
       if msgs.empty?
-        @mail_to_issue.errors.add_to_base("no such mail #{number}")
+        @mail_to_issue.errors[:base] << "no such mail #{number}"
         render :action => 'new'
       else
         msg = msgs.first.attr['RFC822']
@@ -29,7 +29,7 @@ class MailToIssueController < ApplicationController
         if issue = MailHandler.receive(msg, :issue => {:project => @project.identifier, :tracker => tracker.name}) and issue.kind_of?(Issue)
           redirect_to :controller => 'issues', :action => 'show', :id => issue.id
         else
-          @mail_to_issue.errors.add_to_base("failed to process #{number}")
+          @mail_to_issue.errors[:base] << "failed to process #{number}"
           render :action => 'new'
         end
       end
