b03125548fb89e1fe60b0e7536d2cc6c498a4442
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-01-04 15:35:41 +0900

    patch bugs.r-l.o changes

---
+++ config/additional_environment.rb
@@ -0,0 +1,11 @@
+# Copy this file to additional_environment.rb and add any statements
+# that need to be passed to the Rails::Initializer.  `config` is
+# available in this context.
+#
+# Example:
+#
+#   config.log_level = :debug
+#   ...
+#
+
+config.force_ssl = true if Rails.env.production?
