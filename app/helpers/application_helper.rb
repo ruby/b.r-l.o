d39bc73789bff3aa73cff27057195de35fdb6253
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-01-27 12:00:24 +0900

    only display user reputation with administrator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -426,7 +426,9 @@ module ApplicationHelper
   end
 
   def authoring(created, author, options={})
-    l(options[:label] || :label_added_time_by, :author => "#{link_to_user(author)} (#{author.reputation})", :age => time_tag(created)).html_safe
+    author_label = link_to_user(author)
+    author_label += " (#{author.reputation})" if User.current.admin?
+    l(options[:label] || :label_added_time_by, :author => author_label, :age => time_tag(created)).html_safe
   end
 
   def time_tag(time)
