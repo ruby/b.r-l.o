3bc5d046d2975224db7e5e4a28eedd78e9749fb1
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2019-01-12 10:04:53 +0900

    Refine the previous commit.

--- plugins/redmine_link_to_root/lib/redmine_link_to_root.rb
+++ plugins/redmine_link_to_root/lib/redmine_link_to_root.rb
@@ -1,20 +1,22 @@
 require Rails.root.join('app', 'helpers', 'application_helper')
 
-module RedmineLinkToRoot
+module ActionViewPatch
   include ApplicationHelper
   extend ActiveSupport::Concern
 
   included do
-    alias_method_chain :page_header_title, :root_link
+    prepend RedmineLinkToRoot
   end
 
-  def page_header_title_with_root_link
-    if @project.nil? || @project.new_record?
-      link_to(Setting.app_title, home_path)
-    else
-      page_header_title_without_root_link
+  module RedmineLinkToRoot
+    def page_header_title
+      if @project.nil? || @project.new_record?
+        link_to(Setting.app_title, home_path)
+      else
+        super
+      end
     end
   end
 end
 
-ActionView::Base.send(:include, RedmineLinkToRoot)
+ActionView::Base.include ActionViewPatch
