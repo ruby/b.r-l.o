10d5683273b9706ef74bf86e6ce466024f8fd694
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2019-01-12 18:47:01 +0900

    Fixed wrong method name

--- plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/issues_controller.rb
+++ plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/issues_controller.rb
@@ -1,6 +1,6 @@
 module MailingListIntegrationIssuesController
   private
-  def superupdate_issue_from_params
+  def update_issue_from_params
     if val = super
       @issue.current_journal.originates_from_mail = false
     end
