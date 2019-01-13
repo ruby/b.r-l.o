cb537552545a1f848d85305de5a88e442eea48ad
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2019-01-13 11:55:45 +0900

    Fixed scope and migration for deliver methods.

--- plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/mailer.rb
+++ plugins/redmine_mailing_list_integration/lib/redmine_mailing_list_integration/redmine_ext/mailer.rb
@@ -1,28 +1,4 @@
 module MailingListIntegrationMailer
-  class << self
-    def deliver_issue_add(issue)
-      unless issue.originates_from_mail?
-        to = issue.notified_users
-        cc = issue.notified_watchers - to
-        issue.each_notification(to + cc) do |users|
-          Mailer.issue_add(issue, to & users, cc & users).deliver
-        end
-      end
-    end
-
-    def deliver_issue_edit(journal)
-      unless journal.originates_from_mail?
-        issue = journal.journalized.reload
-        to = journal.notified_users
-        cc = journal.notified_watchers
-        journal.each_notification(to + cc) do |users|
-          issue.each_notification(users) do |users2|
-            Mailer.issue_edit(journal, to & users2, cc & users2).deliver
-          end
-        end
-      end
-    end
-  end
 
   def issue_add(user, issue)
     mailing_lists = issue.project.mail_routes_for_issue(issue)
@@ -76,5 +52,19 @@ module MailingListIntegrationMailer
 end
 
 Mailer.class_eval do
+  class << self
+    def deliver_issue_add(issue)
+      unless issue.originates_from_mail?
+        issue_add(issue.author, issue).deliver_later
+      end
+    end
+
+    def deliver_issue_edit(journal)
+      unless journal.originates_from_mail?
+        issue_edit(journal.author, journal).deliver_later
+      end
+    end
+  end
+
   prepend MailingListIntegrationMailer
 end
