2be520ecd2bf1d1b609841a1d556be0cc87a53a7
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-05-18 14:57:26 +0900

    Added migration for issue.lang.

---
+++ plugins/redmine_ruby_lang_mailing_list_customization/db/migrate/20190518145352_add_issues_lang.rb
@@ -0,0 +1,9 @@
+class AddIssuesClosedOn < ActiveRecord::Migration[5.2]
+  def up
+    add_column :issues, :lang, :string, :limit => 8, :default => "en"
+  end
+
+  def down
+    remove_column :issues, :lang
+  end
+end
