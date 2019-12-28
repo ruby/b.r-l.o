04cd8edc83e06bed42afccfd18bccea35350d40f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-12-28 16:55:10 +0800

    Also include RedmineCrm::FormTagHelper

--- plugins/redmineup_tags/lib/redmineup_tags/hooks/views_layouts_hook.rb
+++ plugins/redmineup_tags/lib/redmineup_tags/hooks/views_layouts_hook.rb
@@ -18,11 +18,13 @@
 # along with redmine_tags.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'redmine_crm/helpers/external_assets_helper'
+require 'redmine_crm/helpers/form_tag_helper'
 
 module RedmineupTags
   module Hooks
     class ViewsLayoutsHook < Redmine::Hook::ViewListener
       include RedmineCrm::ExternalAssetsHelper
+      include RedmineCrm::FormTagHelper
 
       def view_layouts_base_html_head(context = {})
         stylesheet_link_tag "redmine_tags.js", :plugin => 'redmineup_tags'
