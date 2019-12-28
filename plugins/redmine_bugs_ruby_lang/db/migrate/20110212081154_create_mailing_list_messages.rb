ea1dc482d7372f174fc7c7e3d97e0f7cc45d7f41
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-12-29 07:34:53 +0800

    Try to migrate again

--- plugins/redmine_bugs_ruby_lang/db/migrate/20110212081154_create_mailing_list_messages.rb
+++ plugins/redmine_bugs_ruby_lang/db/migrate/20110212081154_create_mailing_list_messages.rb
@@ -1,23 +1,23 @@
 class CreateMailingListMessages < ActiveRecord::Migration[5.2]
   def self.up
-    create_table :mailing_list_messages do |t|
-      t.string :message_id
-      t.string :in_reply_to
-      t.string :archive_url
-      t.text :references
-      t.integer :mail_number
+    # create_table :mailing_list_messages do |t|
+    #   t.string :message_id
+    #   t.string :in_reply_to
+    #   t.string :archive_url
+    #   t.text :references
+    #   t.integer :mail_number
 
-      t.references :issue
-      t.references :journal
-      t.references :mailing_list, null: false
-    end
+    #   t.references :issue
+    #   t.references :journal
+    #   t.references :mailing_list, null: false
+    # end
 
-    add_index :mailing_list_messages, :message_id
-    add_index :mailing_list_messages, :issue_id
-    add_index :mailing_list_messages, :journal_id
-    add_index :mailing_list_messages, [:issue_id, :journal_id]
-    add_index :mailing_list_messages, [:mailing_list_id, :message_id]
-    add_index :mailing_list_messages, [:mailing_list_id, :mail_number]
+    # add_index :mailing_list_messages, :message_id
+    # add_index :mailing_list_messages, :issue_id
+    # add_index :mailing_list_messages, :journal_id
+    # add_index :mailing_list_messages, [:issue_id, :journal_id]
+    # add_index :mailing_list_messages, [:mailing_list_id, :message_id]
+    # add_index :mailing_list_messages, [:mailing_list_id, :mail_number]
   end
 
   def self.down
