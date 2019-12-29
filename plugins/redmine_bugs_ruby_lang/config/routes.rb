e52fd79cd094abb1737b935f149c54d7ff6f09b3
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-12-29 08:35:55 +0800

    Revert "Move custom routes to plugin"
    This reverts commit 0ff975c8b8c2b33815970247b309ab5da3ef3361.

--- plugins/redmine_bugs_ruby_lang/config/routes.rb
+++ plugins/redmine_bugs_ruby_lang/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-
-  get '/projects/ruby-trunk:path', to: redirect('/projects/ruby-master%{path}'), constraints: { path: %r{(|/.*)} }
-
   resources :mailing_lists do
     resources :uses, controller: 'uses_of_mailing_list'
   end
   resources :mail_to_issue, only: [:new, :create]
-
 end
