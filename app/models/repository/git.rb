509d5446a53f330a8c65aebe4839ea3d1c06d8ff
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-01-31 18:25:34 +0900

    Support git repository on heroku

--- app/models/repository/git.rb
+++ app/models/repository/git.rb
@@ -82,6 +82,10 @@ class Repository::Git < Repository
     scm.tags
   end
 
+  def fetch
+    scm.fetch
+  end
+
   def default_branch
     scm.default_branch
   rescue => e
@@ -130,6 +134,8 @@ class Repository::Git < Repository
   # The repository can still be fully reloaded by calling #clear_changesets
   # before fetching changesets (eg. for offline resync)
   def fetch_changesets
+    fetch
+
     scm_brs = branches
     return if scm_brs.nil? || scm_brs.empty?
 
