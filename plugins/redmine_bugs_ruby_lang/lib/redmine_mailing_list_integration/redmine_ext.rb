61ccb6d224d3c9d969393c466d83b8ee46cbb550
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-04-09 08:54:27 +0900

    Integrate Journal extension for Zeitwerk

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/redmine_ext.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/redmine_ext.rb
@@ -1,6 +1,10 @@
 require 'redmine_mailing_list_integration/redmine_ext/issue'
 require 'redmine_mailing_list_integration/redmine_ext/issues_controller'
-require 'redmine_mailing_list_integration/redmine_ext/journal'
+
+Journal.class_eval do
+  include BasedOnMail
+end
+
 require 'redmine_mailing_list_integration/redmine_ext/mail_handler'
 require 'redmine_mailing_list_integration/redmine_ext/mailer'
 require 'redmine_mailing_list_integration/redmine_ext/project'
