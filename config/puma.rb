e7b2017fc53bfa857f1fd8e68c2d445f86a658af
NARUSE, Yui <naruse@airemix.jp>
2022-08-12 05:53:35 +0900

    Update git repository on heroku dyno restart

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,10 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
+# Update git repository on heroku dyno restart
+# https://github.com/ruby/heroku-buildpack-bugs-ruby-lang/blob/master/bin/compile
+system "git -C /app/repos/git/ruby fetch" rescue nil
+
 preload_app!
 
 rackup      DefaultRackup
