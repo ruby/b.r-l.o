8c4ff6f84e59154c8b806b397323963d3f1ef0b8
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-11-18 11:58:21 +0900

    Impl List-ID detection for Google Groups

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/google_groups_driver.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/google_groups_driver.rb
@@ -4,9 +4,18 @@ module RedmineMailingListIntegration
       include TypicalDriver
 
       def likelihood
+        list_id = @email.header["List-ID"].match(/\<(.*)\.ruby\-lang\.org\>/)
+        if list_id && list_id[1] == @mailing_list.identifier && @email.header["X-Google-Group-Id"]
+          EXACTLY_MATCHED
+        else
+          NOT_MATCHED
+        end
       end
 
-      def self.imap_query_for_mail_number(mailing_list, number)
+      def self.imap_query_for_mail_number(mailing_list, id)
+        # TODO: Can not find a way to search by Message-ID in Google Groups
+        # ['HEADER', 'List-ID', "<#{mailing_list.identifier}.ruby-lang.org>", 'HEADER', '', id]
+        []
       end
     end
   end
