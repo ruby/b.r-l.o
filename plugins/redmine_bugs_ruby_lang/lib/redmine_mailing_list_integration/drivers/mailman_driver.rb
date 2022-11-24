5475776b5c081a96db7d6cd1407a0d5bde0d7671
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-11-24 10:36:05 +0900

    Support mailman3 provided by mailmanlist

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
@@ -4,16 +4,38 @@ module RedmineMailingListIntegration
       include TypicalDriver
 
       def likelihood
-        if /\A\d/ =~ @email.header["X-Mailman-Version"].to_s and
-          @email.header["X-ML-Name"].to_s == @mailing_list.identifier
+        if /\A\d/ =~ @email.header["X-Mailman-Version"].to_s && ml_name == @mailing_list.identifier
           EXACTLY_MATCHED
         else
           NOT_MATCHED
         end
       end
 
+      def mail_number
+        if @email.header["X-Mail-Count"].to_s
+          # for mailman2
+          @email.header["X-Mail-Count"].to_s
+        else
+          # for mailman3
+          mail_count = @email.header["Subject"].match(/\[#{ml_name}:(\d+)\].*/)
+          mail_count && mail_count[1]
+        end
+      end
+
+      def ml_name
+        if @email.header["X-ML-Name"].to_s
+          # for mailman2
+          @email.header["X-ML-Name"].to_s
+        else
+          # for mailman3
+          list_id = @email.header["List-Id"].match(/\<(.*)\.ruby\-lang\.org\>/)
+          list_id && list_id[1]
+        end
+      end
+
       def self.imap_query_for_mail_number(mailing_list, number)
-        ['HEADER', 'X-ML-Name', mailing_list.identifier, 'HEADER', 'X-Mail-Count', number.to_i]
+        query = "#{mailing_list.identifier}:#{number}"
+        ['HEADER', ml_name, mailing_list.identifier, 'HEADER', 'Subject', query]
       end
     end
   end
