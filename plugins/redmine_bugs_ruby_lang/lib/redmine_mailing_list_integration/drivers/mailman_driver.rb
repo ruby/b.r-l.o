3cb7aa90c48923bb10fe87c60e3170e4a2002340
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-11-25 15:48:17 +0900

    Remove mailman2 support

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
@@ -4,6 +4,8 @@ module RedmineMailingListIntegration
       include TypicalDriver
 
       def likelihood
+        list_id = @email.header["List-Id"].match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
+        ml_name = list_id && list_id[1]
         if /\A\d/ =~ @email.header["X-Mailman-Version"].to_s && ml_name == @mailing_list.identifier
           EXACTLY_MATCHED
         else
@@ -12,25 +14,8 @@ module RedmineMailingListIntegration
       end
 
       def mail_number
-        if @email.header["X-Mail-Count"].to_s
-          # for mailman2
-          @email.header["X-Mail-Count"].to_s
-        else
-          # for mailman3
-          mail_count = @email.header["Subject"].match(/\[#{ml_name}:(\d+)\].*/)
-          mail_count && mail_count[1]
-        end
-      end
-
-      def ml_name
-        if @email.header["X-ML-Name"].to_s
-          # for mailman2
-          @email.header["X-ML-Name"].to_s
-        else
-          # for mailman3
-          list_id = @email.header["List-Id"].match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
-          list_id && list_id[1]
-        end
+        mail_count = @email.header["Subject"].match(/\[#{ml_name}:(\d+)\].*/)
+        mail_count && mail_count[1]
       end
 
       def self.imap_query_for_mail_number(mailing_list, number)
