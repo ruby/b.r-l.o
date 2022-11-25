dd929dc7ce50b5b7eac8836a70edca35d73b4692
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-11-25 16:40:38 +0900

    define ml_name again

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration/drivers/mailman_driver.rb
@@ -4,9 +4,6 @@ module RedmineMailingListIntegration
       include TypicalDriver
 
       def likelihood
-        list_id = @email.header["List-Id"].to_s.match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
-        Rails.logger.info "list_id: #{list_id}"
-        ml_name = list_id && list_id[1]
         Rails.logger.info "ml_name: #{ml_name}"
         if /\A\d/ =~ @email.header["X-Mailman-Version"].to_s && ml_name == @mailing_list.identifier
           EXACTLY_MATCHED
@@ -19,6 +16,12 @@ module RedmineMailingListIntegration
         mail_count = @email.header["Subject"].to_s.match(/\[#{ml_name}:(\d+)\].*/)
         Rails.logger.info "mail_count: #{mail_count}"
         mail_count && mail_count[1]
+      end
+
+      def ml_name
+        list_id = @email.header["List-Id"].to_s.match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
+        Rails.logger.info "list_id: #{list_id}"
+        ml_name = list_id && list_id[1]
       end
 
       def self.imap_query_for_mail_number(mailing_list, number)
