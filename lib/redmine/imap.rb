af758f2e5fe35e09fb03f2893bec0b80b618409b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-02 21:28:05 +0900

    init recode mailing-list messages

--- lib/redmine/imap.rb
+++ lib/redmine/imap.rb
@@ -18,6 +18,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'net/imap'
+require "aws-sdk-s3"
+require "mail"
 
 module Redmine
   module IMAP
@@ -39,6 +41,9 @@ module Redmine
           msg = imap.uid_fetch(uid,'RFC822')[0].attr['RFC822']
           logger.debug "Receiving message #{uid}" if logger && logger.debug?
           if MailHandler.safe_receive(msg, options)
+
+            record_s3(msg)
+
             logger.debug "Message #{uid} successfully received" if logger && logger.debug?
             if imap_options[:move_on_success]
               imap.uid_copy(uid, imap_options[:move_on_success])
@@ -59,6 +64,40 @@ module Redmine
       end
 
       private
+
+      def record_s3(msg)
+        m = Mail.new(msg)
+        list_name = m.header['List-Id'].to_s.match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
+        list_name = list_name && list_name[1]
+        post_id = m.header["Subject"].to_s.match(/\[#{ml_name}:(\d+)\].*/)
+        post_id = post_id && post_id[1]
+
+        from = begin
+          m.header["from"].to_s.gsub(/@[a-zA-Z.\-]+/, "@...")
+        rescue
+          m.header['from']
+        end
+        io = StringIO.new
+        io.puts "From: #{from}"
+        io.puts "Date: #{m.date}"
+        begin
+          io.puts "Subject: #{m.subject}"
+        rescue Encoding::CompatibilityError
+          io.puts "Subject: "
+        end
+        io.puts ""
+        io.puts m.body.to_s.encode("UTF-8", "ISO-2022-JP", invalid: :replace, undef: :replace)
+
+        s3 = Aws::S3::Resource.new(
+          region: 'ap-northeast-1',
+          access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        )
+        bucket = s3.bucket('blade.ruby-lang.org')
+        bucket.object("#{list_name}/#{post_id}").put(body: io.string)
+      ensure
+        io.close
+      end
 
       def logger
         ::Rails.logger
