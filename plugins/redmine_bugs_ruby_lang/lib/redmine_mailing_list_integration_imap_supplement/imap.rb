b7c611e5870931235611d614fea7b584606f288e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2022-12-08 17:04:59 +0900

    We should move record_s3 under the redmine extension for bugs

--- plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration_imap_supplement/imap.rb
+++ plugins/redmine_bugs_ruby_lang/lib/redmine_mailing_list_integration_imap_supplement/imap.rb
@@ -1,5 +1,8 @@
 require 'net/imap'
 require 'yaml'
+require "aws-sdk-s3"
+require "mail"
+require "stringio"
 
 module RedmineMailingListIntegrationImapSupplement
   module IMAP
@@ -47,7 +50,7 @@ module RedmineMailingListIntegrationImapSupplement
         fetch(storage_name, query, session).each do |data|
           msg = data.attr['RFC822']
           seqno = data.seqno
-          if MailHandler.receive(msg, unknown_user: "accept", no_permission_check: '1')
+          if MailHandler.receive(msg, unknown_user: "accept", no_permission_check: '1') && record_s3(msg)
             logger.debug "Message #{seqno} successfully received" if logger && logger.debug?
             session.store(seqno, "+FLAGS", [:Seen])
           else
@@ -63,6 +66,40 @@ module RedmineMailingListIntegrationImapSupplement
       targets.each do |name|
         receive(name, ['NOT', 'SEEN'])
       end
+    end
+
+    def record_s3(msg)
+      m = Mail.new(msg)
+      list_name = m.header['List-Id'].to_s.match(/\<(.*)\.ml\.ruby\-lang\.org\>/)
+      list_name = list_name && list_name[1]
+      post_id = m.header["Subject"].to_s.match(/\[#{list_name}:(\d+)\].*/)
+      post_id = post_id && post_id[1]
+
+      from = begin
+        m.header["from"].to_s.gsub(/@[a-zA-Z.\-]+/, "@...")
+      rescue
+        m.header['from']
+      end
+      io = StringIO.new
+      io.puts "From: #{from}"
+      io.puts "Date: #{m.date}"
+      begin
+        io.puts "Subject: #{m.subject}"
+      rescue Encoding::CompatibilityError
+        io.puts "Subject: "
+      end
+      io.puts ""
+      io.puts m.body.to_s.encode("UTF-8", "ISO-2022-JP", invalid: :replace, undef: :replace)
+
+      s3 = Aws::S3::Resource.new(
+        region: 'ap-northeast-1',
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      )
+      bucket = s3.bucket('blade.ruby-lang.org')
+      bucket.object("#{list_name}/#{post_id}").put(body: io.string)
+    ensure
+      io.close
     end
 
     def logger
