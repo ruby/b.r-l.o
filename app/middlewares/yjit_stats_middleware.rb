45fb507bdea99c8bdb0719e6caa53dc6089c851a
Takashi Kokubun <takashikkbn@gmail.com>
2023-12-16 00:47:28 -0800

    Emit more YJIT metrics

--- app/middlewares/yjit_stats_middleware.rb
+++ app/middlewares/yjit_stats_middleware.rb
@@ -1,26 +1,55 @@
 class YjitStatsMiddleware
+  YJIT_STATS_REQUEST_INTERVAL = 10
   CUSTOM_METRICS = [
     :code_region_size,
     :yjit_alloc_size,
+    :live_iseq_count,
+    :cold_iseq_entry,
+    :compiled_iseq_entry,
+    :compiled_iseq_count,
+    :compiled_blockid_count,
+    :compiled_block_count,
+    :compiled_branch_count,
   ]
 
-  def initialize(app)
+  def initialize(app, logger: Rails.logger)
     @app = app
+    @count = 0
+    @logger = logger
   end
 
   def call(env)
-    if defined?(RubyVM::YJIT) && RubyVM::YJIT.enabled?
-      record_yjit_stats
+    @count += 1
+    with_yjit_stats do
+      @app.call(env)
     end
-    @app.call(env)
   end
 
   private
 
-  def record_yjit_stats
-    stats = RubyVM::YJIT.runtime_stats
-    CUSTOM_METRICS.each do |metric|
-      NewRelic::Agent.record_metric("Custom/YJIT/#{metric}", stats[metric])
+  def with_yjit_stats
+    request_start_ms = Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond)
+    yield
+  ensure
+    begin
+      if request_start_ms
+        request_time_ms = Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_millisecond) - request_start_ms
+        NewRelic::Agent.record_metric('Custom/Unicorn/request_time_ms', request_time_ms)
+      end
+      NewRelic::Agent.record_metric('Custom/Unicorn/request_count', @count)
+
+      if using_yjit? && (@count % YJIT_STATS_REQUEST_INTERVAL) == 0
+        stats = RubyVM::YJIT.runtime_stats
+        CUSTOM_METRICS.each do |metric|
+          NewRelic::Agent.record_metric("Custom/YJIT/#{metric}", stats[metric])
+        end
+      end
+    rescue => e
+      @logger.error(e.full_message)
     end
+  end
+
+  def using_yjit?
+    defined?(RubyVM::YJIT) && RubyVM::YJIT.enabled?
   end
 end
