4210715d1c53198f27be6002246fa8867eb70cdd
NARUSE, Yui <naruse@airemix.jp>
2025-12-14 05:18:49 +0900

    Enable profiler

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,5 +2,9 @@ require 'pg'
 require 'datadog'
 
 Datadog.configure do |c|
+  c.profiling.enabled = true
+  c.env = 'prod'
+  c.service = 'redmine_app'
+  c.version = ENV['HEROKU_RELEASE_VERSION']
   c.tracing.instrument :pg
 end
